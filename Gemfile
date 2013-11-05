419c394bf4b70f50c99782ad9965dc7fbeff1a82
Kilen <zhangzhilin@mapgis.com>
2013-11-05 23:08:16 +0800

    page show tap water notices

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :doc do
 end
 
 gem 'nokogiri'
+gem 'pry'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,17 @@
+require "open-uri"
 class PageController < ApplicationController
   def index
+    @url = "http://www.whwater.com/news/tstz/"
+    @notices = fetch_notices
+  end
+
+  private
+  #return format: [
+  #  { time: "xxxx", event: "xxx" }
+  #]
+  def fetch_notices
+    Nokogiri::HTML(open(@url)).css(".STYLE8").map do |item|
+      { event: item.text, time: item.next.next.text }
+    end
   end
 end
