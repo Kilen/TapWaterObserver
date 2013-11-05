2d84c8c10cc1c52f546444ffa8c7c0e285c82b30
Kilen <zhangzhilin@mapgis.com>
2013-11-05 22:45:02 +0800

    initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TapWaterObserver::Application.config.secret_key_base = 'ba40a6369530934d0d132953d87e9793f3beb73889258f902fa522f07b5e96e9d38f09cce2dc0ad74b68149d022c7c0d992bb10bc1351481ccd77541efc6b551'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TapWaterObserver::Application.config.session_store :cookie_store, key: '_tap_water_observer_session'
