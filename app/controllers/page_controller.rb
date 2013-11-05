050fcaeab1bbe10a55e85f1c9175fde9fe145240
Kilen <zhangzhilin@mapgis.com>
2013-11-05 23:17:07 +0800

    show info according to different type

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,15 +2,16 @@ require "open-uri"
 class PageController < ApplicationController
   def index
     @url = "http://www.whwater.com/news/tstz/"
-    @notices = fetch_notices
+    @planned_notices = fetch_notices "http://www.whwater.com/news/jhxts/Index.html"
+    @emergency_notices = fetch_notices "http://www.whwater.com/news/tfxts/Index.html"
   end
 
   private
   #return format: [
   #  { time: "xxxx", event: "xxx" }
   #]
-  def fetch_notices
-    Nokogiri::HTML(open(@url)).css(".STYLE8").map do |item|
+  def fetch_notices(url)
+    Nokogiri::HTML(open(url)).css(".STYLE8").map do |item|
       { event: item.text, time: item.next.next.text }
     end
   end
